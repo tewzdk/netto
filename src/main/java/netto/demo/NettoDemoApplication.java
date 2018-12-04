23ff8169632b732ba3790d144de5f81713563191
Sidney Schultz <tewzdk@gmail.com>
2018-12-04 12:28:13 +0100

    MVC

---
+++ src/main/java/netto/demo/NettoDemoApplication.java
@@ -0,0 +1,12 @@
+package netto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettoDemoApplication.class, args);
+    }
+}
